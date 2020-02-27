f62817d2c66d813941da6246551898d842d68f32
LeoKnox <LeoKnox@users.noreply.github.com>
2020-02-26 19:37:16 -0700

    Add files via upload

---
+++ Models/RoomHeightGroup.cs/RoomHeightGroup.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcRoom.Models.RoomHeightGroup
+{
+    public class RoomHeightGroup
+    {
+        public int? RoomHeight { get; set; }
+
+        public int RoomCount { get; set; }
+    }
+}
---
+++ Pages/About.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MvcRoom.Models.RoomHeightGroup;
+using MvcRoom.Data;
+
+namespace MvcRoom.Pages
+{
+    public class AboutModel : PageModel
+    {
+        private readonly MvcRoomContext _context;
+
+        public AboutModel(MvcRoomContext context)
+        {
+            _context = context;
+        }
+
+        public IList<RoomHeightGroup> Room { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            IQueryable<RoomHeightGroup> data =
+                from room in _context.Room
+                group room by room.Length into roomGroup
+                select new RoomHeightGroup()
+                {
+                    RoomHeight = roomGroup.Key,
+                    RoomCount = roomGroup.Count()
+                };
+
+            Room = await data.AsNoTracking().ToListAsync();
+        }
+    }
+}
