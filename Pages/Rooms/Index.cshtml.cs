aa2c92e8b4723ac12ab8253f48623d6e1d85f219
LeoKnox <LeoKnox@users.noreply.github.com>
2020-02-26 17:24:18 -0700

    Add files via upload

--- Pages/Rooms/Index.cshtml.cs
+++ Pages/Rooms/Index.cshtml.cs
@@ -12,18 +12,37 @@ namespace MvcRoom
 {
     public class IndexModel : PageModel
     {
-        private readonly MvcRoom.Data.MvcRoomContext _context;
+        private readonly MvcRoomContext _context;
 
-        public IndexModel(MvcRoom.Data.MvcRoomContext context)
+        public IndexModel(MvcRoomContext context)
         {
             _context = context;
         }
 
+        public string NameSort { get; set; }
+        public string CurrentFilter { get; set; }
+        public string CurrentSort { get; set; }
+
         public IList<Room> Room { get;set; }
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(string sortOrder)
         {
-            Room = await _context.Room.ToListAsync();
+            NameSort = String.IsNullOrEmpty(sortOrder) ? "name_des" : "";
+
+            IQueryable<Room> roomsIQ = from r in _context.Room
+                                       select r;
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    roomsIQ = roomsIQ.OrderByDescending(r => r.Name);
+                    break;
+                default:
+                    roomsIQ = roomsIQ.OrderBy(r => r.Name);
+                    break;
+            }
+
+            Room = await roomsIQ.AsNoTracking().ToListAsync();
         }
     }
 }
