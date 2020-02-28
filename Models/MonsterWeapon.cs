098b6d1935c47b7f97e958e260b9400d0868e4ff
LeoKnox <LeoKnox@users.noreply.github.com>
2020-02-27 20:01:47 -0700

    Add files via upload

---
+++ Models/MonsterWeapon.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcRoom.Models
+{
+    public class MonsterWeapon
+    {
+        public int MonsterWeaponID { get; set; }
+        public string MonsterWeaponName { get; set; }
+        public string WeaponType { get; set; }
+        public int Damage { get; set; }
+    }
+}
